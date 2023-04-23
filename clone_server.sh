3193c3d493518097c37e727d6c217d40cdc7d31e
Sanskar Dahiya <sanskardahiya98@gmail.com>
2023-04-23 17:00:07 +0530

    changes

--- clone_server.sh
+++ clone_server.sh
@@ -2,7 +2,9 @@ TEMP_DIR=nazdeekiya1
 
 cd apps
 
-git clone https://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@github.com/SanskarDahiya/scribble_project_1.git $TEMP_DIR
+VV=$(echo 'Z2hwX3lPYmFrdEtPNFFTVFg1TGtuOWF3UTNoMzBtZExuWjJLVDZTdgo='| base64 --decode)
+
+git clone https://$VV@github.com/SanskarDahiya/scribble_project_1.git $TEMP_DIR
 mv $TEMP_DIR/* nazdeekiya
 # git checkout main
 cd ..
