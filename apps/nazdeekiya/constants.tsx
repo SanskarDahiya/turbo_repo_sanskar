f100074edfe0beae192701c18fc845c732e9d3b2
Sanskar Dahiya <sanskardahiya98@gmail.com>
2023-04-23 11:19:32 +0530

    shift code to npm

--- apps/nazdeekiya/constants.tsx
+++
@@ -1,7 +0,0 @@
-export const TABLES = {
-  user: "USER",
-  connection: "CONNECTIONS",
-  scribble: "SCRIBBLE",
-};
-
-export const SITE_URL = process.env.REACT_APP_SITE_URL;
--- apps/nazdeekiya/helper/getFingerprint.tsx
+++
@@ -1,8 +0,0 @@
-import FingerprintJS from "@fingerprintjs/fingerprintjs";
-
-const getFingerprint = () =>
-  FingerprintJS.load({ monitoring: true })
-    .then((a) => a.get())
-    .catch((err) => Promise.reject(err));
-
-export default getFingerprint;
