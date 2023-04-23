8caa21511f7967cc5753c5ca9c0bca0287a60f55
Sanskar Dahiya <sanskardahiya98@gmail.com>
2023-04-23 11:47:58 +0530

    lets hope this work

--- clone_server.sh
+++ clone_server.sh
@@ -1,5 +1,8 @@
+TEMP_DIR=nazdeekiya1
+
 cd apps
-git clone https://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@github.com/SanskarDahiya/scribble_project_1.git nazdeekiya1
-mv nazdeekiya1 nazdeekiya
+
+git clone https://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@github.com/SanskarDahiya/scribble_project_1.git $TEMP_DIR
+mv $TEMP_DIR/* nazdeekiya
 # git checkout main
 cd ..
