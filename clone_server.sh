239579cc4f8bb8b60fa22cd8d826fd6a4dd98c1e
Sanskar Dahiya <sanskardahiya98@gmail.com>
2023-04-23 17:14:56 +0530

    changes for gitkeep dir

--- clone_server.sh
+++ clone_server.sh
@@ -1,11 +1,12 @@
 TEMP_DIR=nazdeekiya1
 
 cd apps
+mkdir -p nazdeekiya
+touch nazdeekiya/.gitkeep
+git clone https://github.com/SanskarDahiya/scribble_project_1.git $TEMP_DIR
+mv $TEMP_DIR/* $TEMP_DIR/.* nazdeekiya
 
-VV=$(echo 'Z2hwX3lPYmFrdEtPNFFTVFg1TGtuOWF3UTNoMzBtZExuWjJLVDZTdgo='| base64 --decode)
 
-git clone https://$VV@github.com/SanskarDahiya/scribble_project_1.git $TEMP_DIR
-mv $TEMP_DIR/* nazdeekiya
 echo ">"$(cat nazdeekiya/package.json | grep "version")
 # git checkout main
 cd ..
