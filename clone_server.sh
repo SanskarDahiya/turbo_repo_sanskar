84bd2cb7256e230caeee8c24931f3182bf3f6db6
Sanskar Dahiya <sanskardahiya98@gmail.com>
2023-04-23 17:04:19 +0530

    echo version name for clarify

--- clone_server.sh
+++ clone_server.sh
@@ -6,5 +6,6 @@ VV=$(echo 'Z2hwX3lPYmFrdEtPNFFTVFg1TGtuOWF3UTNoMzBtZExuWjJLVDZTdgo='| base64 --d
 
 git clone https://$VV@github.com/SanskarDahiya/scribble_project_1.git $TEMP_DIR
 mv $TEMP_DIR/* nazdeekiya
+echo ">"$(cat nazdeekiya/package.json | grep "version")
 # git checkout main
 cd ..
